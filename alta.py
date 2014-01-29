4f7b577e78d6ec5b88417631ba7e041cff865198
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-01-29 09:20:02 +0100

    actualizacion comprobacion de usuarios

--- alta.py
+++ alta.py
@@ -1,21 +1,28 @@
 import commands
 import sys
 import os
+import MySQLdb
 #pedimos el nombre
 nombre = raw_input("introduce el nombre: ")
-
-#vemos si el usuario existe buscamos el nombre en la carpeta que se llamara 
-#como el nombre de dominio
-os.system("ls /home/sergio/pruebahosting > usuarios.txt")
-#el fichero se crea donde ejecutamos el programa
-f=open("/home/sergio/hosting/usuarios.txt","r")
-fichero=f.readlines()
-f.close()
-print fichero
-print fichero[0]
-#for linea in fichero:
-#	print linea
-#	if nombre==linea:
-#		print "nombre existente"
-#print carpetas
-
+dominio = raw_input("introduce el nombre de dominio: ")
+#abrimos una conexion
+base = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="usuarios")
+cursor=base.cursor()
+#vemos si el usuario existe buscamos en mysql el nombre de usuario
+consultausuario="select nombre from usuarios where nombre='%s';" %nombre
+cursor.execute(consultausuario)
+consulta_usuario = cursor.fetchone()
+if consulta_usuario !=None:
+	print "nombre de usuario existente"
+	sys.exit
+else:
+#vemos si existe el dominio que tambien lo guardaremos en mysql
+	consultadominio="select dominio from usuarios where dominio='%s';" %dominio
+	cursor.execute(consultadominio)
+	consulta_dominio = cursor.fetchone()
+	if consulta_dominio !=None:
+		print "nombre de dominio existente"
+		sys.exit
+	else:
+#siguiente paso
+		print "hola"
