6d09d465e9c1b63310bcf311eb245776c9bb6589
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-02-06 17:05:40 +0100

    varios puntos hechos

--- baja.py
+++ baja.py
@@ -11,11 +11,33 @@ cursor=base.cursor()
 consultausuario="select ndominio from usuarios where ndominio='%s';" %dominio
 cursor.execute(consultausuario)
 #consulta_usuario = cursor.fetchone()
-if consulta_usuario == None:
+if consultausuario == None:
 	print "El usuario %s no existe"%nombre
 	sys.exit
 else:
 #borramos la carpeta personal
 	os.system("rm -r /srv/www/%s" % nombre)
-	os.system("a2dissite %s > /dev/null" % nombre)
-    	
+#desabilitamos los virtualhost
+	os.system("a2dissite www.%s>/dev/null"%dominio)
+        os.system("a2dissite mysql.%s>/dev/null"%dominio)
+#borramos los virtualhosts
+	os.system("rm -r /etc/apache2/sites-available/www.%s"%dominio)
+	os.system("rm -r /etc/apache2/sites-available/mysql.%s"%dominio)
+#reiniciamos apache
+	os.system("service apache2 restart>/dev/null")
+#borramos el usuario de mysql y las bases de datos
+	borrarbases="drop database my%s"%nombre
+	cursor.execute(borrarbases)
+	base.commit()
+	borrausuario=" drop user my%s@localhost" %nombre 
+    	cursor.execute(borrausuario)
+    	base.commit()
+#borramos la columna de la tabla ftpd
+	borracolumna="delete from usuarios where ndominio='%s';" %nombre
+    	cursor.execute(borracolumna)
+   	base.commit()
+#borramos el fichero db. donde 
+	os.system("rm -r /etc/bind/%s" %dominio)
+#borramos la zona
+
+
