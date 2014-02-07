93aa4617cc4e7c1b55b1b61741b6751f0cb84019
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-02-07 12:18:46 +0100

    programa con sys.argv

--- change_password.py
+++ change_password.py
@@ -1,16 +1,16 @@
 import MySQLdb
 import sys
 import getpass
-print "si introduce mysql cambiara la contrasenna de el usuario mysql o puede introducir ftp para cambiar la de el usuario ftp"
-accion=raw_input("escriba su operacion a realizar: ")
+import sys
+accion=(sys.argv[1])
 base = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="mysql")
 cursor=base.cursor()
 #si la accion pedida es mysql
 if accion == "mysql":
 	print "procederemos a cambiar la contrasenna de mysql"
 #pedimos el usuario para el que vamos a cambiar la contrasenna y la contrasenna antigua
-	usuariomysql=raw_input("introduzca su usuario mysql: ")
-	contrasenaantigua=getpass.getpass("introduzca su contrasenna antigua mysql: ")
+	usuariomysql=(sys.argv[2])
+	contrasenaantigua=(sys.argv[3])
 #codificamos la contrasenna igual que en mysql
 	base2 = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="cambiocontrasenna")
 	cursor2=base2.cursor()
@@ -34,14 +34,12 @@ if accion == "mysql":
 #si la contrasenna y el usuario coinciden procedemos a cambiarla
 	if consulta_usuario[0]== usuariomysql and consulta_contrasenna[0]== consultacodificada[0]:
 		print "usuario y contrasenna correctas"
-#pedimos dos veces la contrasenna
-		contrasenanueva=getpass.getpass("introduzca su nueva contrasenna: ")
-		pruebadenuevo=getpass.getpass("introduzca de nuevo la contrasenna: ")
-		if contrasenanueva == pruebadenuevo:
-			cambio="SET PASSWORD FOR "+usuariomysql+"@localhost = PASSWORD('"+contrasenanueva+"');"
-			cursor.execute(cambio)
-			base.commit()
-			print "contrasenna actualizada correctamente"
+#contrasenna nueva
+		contrasenanueva=(sys.argv[4])
+		cambio="SET PASSWORD FOR "+usuariomysql+"@localhost = PASSWORD('"+contrasenanueva+"');"
+		cursor.execute(cambio)
+		base.commit()
+		print "contrasenna actualizada correctamente"
 #borramos el registro creado para que la tabla no aumente
 			base2 = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="cambiocontrasenna")
         		cursor2=base2.cursor()
@@ -66,9 +64,9 @@ elif accion == "ftp":
 	base3 = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="ftpd")
 	cursor3=base3.cursor()
 	print "procederemos a cambiar la contrasenna de ftp"
-#pedimos el usuario para el que vamos a cambiar la contrasenna y la contrasenna antigua
-        usuarioftp=raw_input("introduzca su usuario ftp: ")
-        contrasenaantigua=getpass.getpass("introduzca su contrasenna antigua ftp: ")
+#vemos el usuario para el que vamos a cambiar la contrasenna y la contrasenna antigua
+        usuarioftp=(sys.argv[2])
+        contrasenaantigua=(sys.argv[3])
 #codificamos la contrasenna igual que en la contrasenna se guarda en la tabla ftpd
         base2 = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="cambiocontrasenna")
         cursor2=base2.cursor()
@@ -92,9 +90,8 @@ elif accion == "ftp":
 #si la contrasenna y el usuario coinciden procedemos a cambiarla
         if consulta_usuario[0]== usuarioftp and consulta_contrasenna[0]== consultacodificada[0]:
                 print "usuario y contrasenna correctas"
-#pedimos dos veces la contrasenna
-                contrasenanueva=getpass.getpass("introduzca su nueva contrasenna: ")
-                pruebadenuevo=getpass.getpass("introduzca de nuevo la contrasenna: ")
+# contrasenna nueva
+                contrasenanueva=(sys.arg[4])
                 if contrasenanueva == pruebadenuevo:
                         cambio="update usuarios set password = PASSWORD('"+contrasenanueva+"')where username='"+usuarioftp+"';"
                         cursor3.execute(cambio)
@@ -121,4 +118,5 @@ elif accion == "ftp":
                 sys.exit
 	
 else:
-	print "opcion",accion,"incorrecta introduzca fpt o mysql"
+	print "si introduce mysql como primer parametro cambiara la contrasenna de el usuario mysql ademas como segundo el usuario,tercer parametro la contrasenna antigua y como cuarto la nueva contrasenna o puede introducir ftp para cambiar la de el usuario ftp y los parametros usuario, antigua y nueva"
+ 
