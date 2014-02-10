233097931895033faa1c497f63af4dde74957078
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-02-10 14:12:15 +0100

    baja terminado

--- baja.py
+++ baja.py
@@ -1,7 +1,8 @@
 import MySQLdb
 import sys
+import os
 #pedimos el nombre
-dominio = raw_input("introduce el nombre de dominio: ")
+dominio = (sys.argv[1])
 cortacadena=dominio.split(".")
 nombre=cortacadena[0]
 #abrimos una conexion
@@ -12,34 +13,35 @@ consultausuario="select ndominio from usuarios where ndominio='%s';" %dominio
 cursor.execute(consultausuario)
 #consulta_usuario = cursor.fetchone()
 if consultausuario == None:
-	print "El usuario %s no existe"%nombre
-	sys.exit
+        print "El usuario %s no existe"%nombre
+        sys.exit
 else:
 #borramos la carpeta personal
-	os.system("rm -r /srv/www/%s" % nombre)
+        os.system("rm -r /srv/www/%s" % nombre)
 #desabilitamos los virtualhost
-	os.system("a2dissite www.%s>/dev/null"%dominio)
+        os.system("a2dissite www.%s>/dev/null"%dominio)
         os.system("a2dissite mysql.%s>/dev/null"%dominio)
 #borramos los virtualhosts
-	os.system("rm -r /etc/apache2/sites-available/www.%s"%dominio)
-	os.system("rm -r /etc/apache2/sites-available/mysql.%s"%dominio)
+        os.system("rm -r /etc/apache2/sites-available/www.%s"%dominio)
+        os.system("rm -r /etc/apache2/sites-available/mysql.%s"%dominio)
 #reiniciamos apache
-	os.system("service apache2 restart>/dev/null")
+        os.system("service apache2 restart>/dev/null")
 #borramos el usuario de mysql y las bases de datos
-	borrarbases="drop database my%s"%nombre
-	cursor.execute(borrarbases)
-	base.commit()
-	borrausuario=" drop user my%s@localhost" %nombre 
-    	cursor.execute(borrausuario)
-    	base.commit()
+        borrarbases="drop database %s"%nombre
+        cursor.execute(borrarbases)
+        base.commit()
+        borrausuario=" drop user my%s@localhost" %nombre
+        cursor.execute(borrausuario)
+        base.commit()
 #borramos la columna de la tabla ftpd
-	borracolumna="delete from usuarios where ndominio='%s';" %nombre
-    	cursor.execute(borracolumna)
-   	base.commit()
+        borracolumna="delete from usuarios where ndominio='%s';" %dominio
+        cursor.execute(borracolumna)
+        base.commit()
+        base.close()
 #borramos el fichero db. donde 
-	os.system("rm -r /etc/bind/db.%s" %dominio)
+        os.system("rm -r /etc/bind/db.%s" %dominio)
 #borramos la zona del fichero named.conf.local
-	os.system("sed '/zone " + '"%s"'% dominio + "/,/};/d' /etc/bind/named.conf.local > temporal")
-	os.system("mv temporal /etc/bind/named.conf.local")
-	print "El usuario fue elimindado correctamente"
+        os.system("sed '/zone " + '"%s"'% dominio + "/,/};/d' /etc/bind/named.conf.local > temporal")
+        os.system("mv temporal /etc/bind/named.conf.local")
+        print "El usuario fue elimindado correctamente"
 
