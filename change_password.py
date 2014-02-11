dcde24700796a518dfba2756a3d1c01458e4aa37
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-02-11 13:06:10 +0100

    terminado y comprobado

--- change_password.py
+++ change_password.py
@@ -6,7 +6,6 @@ usuario=(sys.argv[2])
 contrasenanueva=(sys.argv[3])
 #si la accion pedida es mysql
 if accion == "mysql":
-        print "procederemos a cambiar la contrasenna de mysql"
         base = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="mysql")
         cursor=base.cursor()
 #buscamos si existe el usuario
@@ -26,7 +25,6 @@ if accion == "mysql":
 elif accion == "ftp":
         base3 = MySQLdb.connect(host="localhost", user="root", passwd="sergio", db="ftpd")
         cursor3=base3.cursor()
-        print "procederemos a cambiar la contrasenna de ftp"
 #buscamos si existe el usuario
         consultausuario="select username from usuarios where username='"+usuario+"';"
         cursor3.execute(consultausuario)
@@ -34,7 +32,6 @@ elif accion == "ftp":
 
 #si el usuario coinciden procedemos a cambiarla
         if consulta_usuario[0]== usuario:
-                print "usuario y contrasenna correctas"
                 cambio="update usuarios set password = PASSWORD('"+contrasenanueva+"')where username='"+usuario+"';"
                 cursor3.execute(cambio)
                 base3.commit()
