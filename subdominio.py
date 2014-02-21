ee3dc2a17bc88e1b9bc4e2ddf71561866631f255
sergiomarchena <ortonchico_sergio@hotmail.com>
2014-02-21 09:36:00 +0100

    añadido alias

--- subdominio.py
+++ subdominio.py
@@ -14,42 +14,47 @@ if resultado == None:
     print "Lo sentimos, el usuario %s no exite" % (nombre)
 else:
 #creamos la capeta del subdominio
-    os.system("mkdir /srv/www/%s/subdominio" % (nombre))
+    	os.system("mkdir /srv/www/%s/subdominio" % (nombre))
 #creamos la carpeta con el nombre del subdominio
-    os.system("mkdir /srv/www/%s/subdominio/%s" % (nombre,subdominio))
+    	os.system("mkdir /srv/www/%s/subdominio/%s" % (nombre,subdominio))
 #creamos el fichero de ejemplo    
-    os.system("echo Pagina de %s en construccion  > /srv/www/%s/subdominio/%s/index.html" %(nombre,nombre,subdominio))
+    	os.system("echo Pagina de %s en construccion  > /srv/www/%s/subdominio/%s/index.html" %(nombre,nombre,subdominio))
 #modificamos el fichero virtualhost creado nuevo y modificamos el primer parametro    
-    virtual_host="/home/debian/plantillas_hosting/virtual_hostsubdominio"
-    virtual=open(virtual_host, "r")
-    filew = open(virtual_host+'.mod', "w")
-    buff = virtual.read()
-    char1='%nombre%'
-    rbuff = buff.replace(char1, nombre)
-    filew.write(rbuff)
-    virtual.close()
-    filew.close()
+    	virtual_host="/home/debian/plantillas_hosting/virtual_hostsubdominio"
+	virtual=open(virtual_host, "r")
+    	filew = open(virtual_host+'.mod', "w")
+    	buff = virtual.read()
+    	char1='%nombre%'
+    	rbuff = buff.replace(char1, nombre)
+    	filew.write(rbuff)
+    	virtual.close()
+    	filew.close()
 #modificamos el fichero modificado otra vez para el dominio
-    virtual_host="/home/debian/plantillas_hosting/virtual_hostsubdominio.mod"
-    virtual=open(virtual_host, "r")
-    filew = open(virtual_host+'.mod', "w")
-    buff = virtual.read()
-    char1='%subdominio%'
-    rbuff = buff.replace(char1, subdominio)
-    filew.write(rbuff)
-    virtual.close()
-    filew.close()
+    	virtual_host="/home/debian/plantillas_hosting/virtual_hostsubdominio.mod"
+    	virtual=open(virtual_host, "r")
+    	filew = open(virtual_host+'.mod', "w")
+    	buff = virtual.read()
+    	char1='%subdominio%'
+    	rbuff = buff.replace(char1, subdominio)
+    	filew.write(rbuff)
+    	virtual.close()
+    	filew.close()
 #borramos el primer fichero modificado que se ha creado 
-    os.system("rm -r /home/debian/plantillas_hosting/virtual_hostsubdominio.mod")
+    	os.system("rm -r /home/debian/plantillas_hosting/virtual_hostsubdominio.mod")
 #movemos el fichero a apache
-    os.system("mv /home/debian/plantillas_hosting/virtual_hostsubdominio.mod.mod /etc/apache2/sites-available/%s"%subdominio)
+    	os.system("mv /home/debian/plantillas_hosting/virtual_hostsubdominio.mod.mod /etc/apache2/sites-available/%s"%subdominio)
 #activamos el dominio
-    os.system("a2ensite %s>/dev/null" % subdominio)
-    os.system("service apache2 restart>/dev/null ")
+    	os.system("a2ensite %s>/dev/null" % subdominio)
+    	os.system("service apache2 restart>/dev/null ")
 #añadimos la resolucion directa
-    directa = open("/etc/bind/db.%s.com" % nombre,"a")
-    directa.write("%s  CNAME s1\n" % subdominio)
-    directa.close()
-    os.system("service bind9 restart>/dev/null ")
-    print "El subdominio fue creado correctamente"
+    	directa = open("/etc/bind/db.%s.com" % nombre,"a")
+    	directa.write("%s  CNAME s1\n" % subdominio)
+    	directa.close()
+    	os.system("service bind9 restart>/dev/null ")
+#annadimos un alias al host principal para que no pueda entrar /subdominio/subdominio
+    	db = open("/etc/apache2/sites-available/www.%s.com" % nombre,"a")
+ 	db.write("Alias /subdominio/%s/ \"/usr/share/apache2/error/HTTP_NOT_FOUND.html.var\" "%subdominio)
+	db.close()
+        os.system("service apache2 restart >/dev/null")
+        print "El subdominio fue creado correctamente"
 
